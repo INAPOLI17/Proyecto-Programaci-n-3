75943b3c77bb5b037413a126b3a7b0c31aabbbcf
INAPOLI17 <apolinarar72@gmail.com>
2022-03-16 21:39:30 -0400

    Apolinar
    Inicio

---
+++ Proyecto Programación Vellonera/Proyecto Programación Vellonera/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Programación_Vellonera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

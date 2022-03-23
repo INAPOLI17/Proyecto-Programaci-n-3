61c04f9007c43737f596b04ed9ea1fb5ed5ca4c3
rayUrena <r.urena1@gmail.com>
2022-03-23 01:04:03 -0400

    Formulario Musica
    Musica

--- Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Musica.Designer.cs
+++ Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Musica.Designer.cs
@@ -28,12 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIdCancion = new System.Windows.Forms.TextBox();
+            this.txtNomCan = new System.Windows.Forms.TextBox();
+            this.txtCanArt = new System.Windows.Forms.TextBox();
+            this.txtAlbCan = new System.Windows.Forms.TextBox();
+            this.txtCanLin = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mantenimiento Música";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id Canción";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Título";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Artista";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Album";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ubicación";
+            // 
+            // txtIdCancion
+            // 
+            this.txtIdCancion.Location = new System.Drawing.Point(97, 77);
+            this.txtIdCancion.Name = "txtIdCancion";
+            this.txtIdCancion.Size = new System.Drawing.Size(100, 23);
+            this.txtIdCancion.TabIndex = 6;
+            // 
+            // txtNomCan
+            // 
+            this.txtNomCan.Location = new System.Drawing.Point(97, 115);
+            this.txtNomCan.Name = "txtNomCan";
+            this.txtNomCan.Size = new System.Drawing.Size(100, 23);
+            this.txtNomCan.TabIndex = 7;
+            // 
+            // txtCanArt
+            // 
+            this.txtCanArt.Location = new System.Drawing.Point(95, 147);
+            this.txtCanArt.Name = "txtCanArt";
+            this.txtCanArt.Size = new System.Drawing.Size(100, 23);
+            this.txtCanArt.TabIndex = 8;
+            // 
+            // txtAlbCan
+            // 
+            this.txtAlbCan.Location = new System.Drawing.Point(95, 179);
+            this.txtAlbCan.Name = "txtAlbCan";
+            this.txtAlbCan.Size = new System.Drawing.Size(100, 23);
+            this.txtAlbCan.TabIndex = 9;
+            // 
+            // txtCanLin
+            // 
+            this.txtCanLin.Location = new System.Drawing.Point(95, 211);
+            this.txtCanLin.Name = "txtCanLin";
+            this.txtCanLin.Size = new System.Drawing.Size(100, 23);
+            this.txtCanLin.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(228, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(397, 166);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(407, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Musica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(637, 322);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtCanLin);
+            this.Controls.Add(this.txtAlbCan);
+            this.Controls.Add(this.txtCanArt);
+            this.Controls.Add(this.txtNomCan);
+            this.Controls.Add(this.txtIdCancion);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Musica";
             this.Text = "Musica";
+            this.Load += new System.EventHandler(this.Musica_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtIdCancion;
+        private TextBox txtNomCan;
+        private TextBox txtCanArt;
+        private TextBox txtAlbCan;
+        private TextBox txtCanLin;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
     }
 }
--- Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Musica.cs
+++ Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Musica.cs
@@ -12,9 +12,39 @@ namespace Proyecto_Programación_Vellonera
 {
     public partial class Musica : Form
     {
+        ClaDatos cladatos = new ClaDatos();
+        Conexión con = new Conexión();
+
         public Musica()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtIdCancion.Clear();
+            txtNomCan.Clear();
+            txtCanArt.Clear();
+            txtAlbCan.Clear();
+            txtCanLin.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+            
+
+        }
+
+        private void Musica_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = con.consulta("Select idCan as ID, nomCan as Título, artCan as Artista, albCan as Album, linCan as Ubicación from Cancion");
         }
     }
 }

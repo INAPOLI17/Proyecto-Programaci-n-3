0071fe5cad4ec448ae4af51ec722311b69e7194f
rayUrena <r.urena1@gmail.com>
2022-03-23 00:02:56 -0400

    Form Cliente
    Trabajando form cliente

--- Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Cliente.Designer.cs
+++ Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Cliente.Designer.cs
@@ -28,35 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvCliente = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIdCliente = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPlataforma = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // dgvCliente
+            // label1
             // 
-            this.dgvCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCliente.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvCliente.Location = new System.Drawing.Point(0, 0);
-            this.dgvCliente.Name = "dgvCliente";
-            this.dgvCliente.RowTemplate.Height = 25;
-            this.dgvCliente.Size = new System.Drawing.Size(800, 450);
-            this.dgvCliente.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Plataforma";
+            // 
+            // txtIdCliente
+            // 
+            this.txtIdCliente.Location = new System.Drawing.Point(156, 60);
+            this.txtIdCliente.Name = "txtIdCliente";
+            this.txtIdCliente.Size = new System.Drawing.Size(100, 23);
+            this.txtIdCliente.TabIndex = 3;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(156, 101);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 23);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtPlataforma
+            // 
+            this.txtPlataforma.Location = new System.Drawing.Point(156, 141);
+            this.txtPlataforma.Name = "txtPlataforma";
+            this.txtPlataforma.Size = new System.Drawing.Size(100, 23);
+            this.txtPlataforma.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Cliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgvCliente);
+            this.ClientSize = new System.Drawing.Size(364, 278);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPlataforma);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtIdCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Cliente";
-            this.Text = "Cliente";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).EndInit();
+            this.Text = "Clientes";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private DataGridView dgvCliente;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtIdCliente;
+        private TextBox txtUsuario;
+        private TextBox txtPlataforma;
+        private Button button1;
+        private Button button2;
     }
 }
--- Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Cliente.cs
+++ Proyecto Programación Vellonera - Copy/Proyecto Programación Vellonera/Cliente.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace Proyecto_Programación_Vellonera
 {
+    
     public partial class Cliente : Form
     {
+        ClaDatos cladatos = new ClaDatos();
+        Conexión con = new Conexión();
         public Cliente()
         {
             InitializeComponent();
+        }
+
+        private void dgvCliente_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtIdCliente.Clear();
+            txtUsuario.Clear();
+            txtPlataforma.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
